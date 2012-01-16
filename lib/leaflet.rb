b1d0f0a1544b524faa00c36f0cfaacee9cc6f151
Charlie Savage <cfis@zerista.com>
2012-01-16 13:31:55 -0800

    Rearrange gem so that the leaflet git submodule can go under library.  Then update the engine paths to be able to use it.  This remove duplication of images and styleshets.

--- lib/leaflet.rb
+++ lib/leaflet.rb
@@ -1,4 +1,18 @@
 module Leaflet
   class Engine < Rails::Engine
+    initializer :append_assets_path do |app|
+      puts 1111111111111
+      puts config.root
+      puts File.join(config.root, 'lib')
+      
+      # For javascripts
+      app.config.assets.paths << File.join(config.root, 'lib', 'leaflet', 'src')
+      
+      # For images and stylesheets
+      app.config.assets.paths << File.join(config.root, 'lib', 'leaflet', 'dist')
+
+      # For additional files
+      app.config.assets.paths << File.join(config.root, 'lib', 'zerista')
+    end
   end
 end
