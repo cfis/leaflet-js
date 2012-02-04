58cad1449c5a52555294fbf2d97fa45cb6f3448d
Charlie Savage <cfis@zerista.com>
2012-02-04 00:22:19 -0700

    Update gemspec to beta4.

--- lib/leaflet-ruby.rb
+++ lib/leaflet-ruby.rb
@@ -1,11 +1,8 @@
 module Leaflet
   class Engine < Rails::Engine
     initializer :append_assets_path do |app|
-      # For javascripts
+      # To link to javascripts
       app.config.assets.paths << File.join(config.root, 'lib', 'leaflet', 'src')
-      
-      # For images and stylesheets
-      app.config.assets.paths << File.join(config.root, 'lib', 'leaflet', 'dist')
 
       # For additional files
       app.config.assets.paths << File.join(config.root, 'lib', 'zerista')
