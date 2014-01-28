160d40a19de185552d21780665bb899f4a1796e2
cjford <cjford@colgate.edu>
2014-01-28 16:33:18 -0700

    Updated submodules and asset require files

--- vendor/assets/javascripts/leaflet.js
+++ vendor/assets/javascripts/leaflet.js
@@ -11,27 +11,28 @@
 //=require ../../../lib/leaflet/src/dom/DomUtil.js
 //=require ../../../lib/leaflet/src/geo/LatLng.js
 //=require ../../../lib/leaflet/src/geo/LatLngBounds.js
-//=require ../../../lib/leaflet/src/geo/projection/Projection.js
-//=require ../../../lib/leaflet/src/geo/projection/Projection.SphericalMercator.js
 //=require ../../../lib/leaflet/src/geo/projection/Projection.LonLat.js
+//=require ../../../lib/leaflet/src/geo/projection/Projection.SphericalMercator.js
 //=require ../../../lib/leaflet/src/geo/crs/CRS.js
 //=require ../../../lib/leaflet/src/geo/crs/CRS.Simple.js
+//=require ../../../lib/leaflet/src/geo/crs/CRS.Earth.js
 //=require ../../../lib/leaflet/src/geo/crs/CRS.EPSG3857.js
 //=require ../../../lib/leaflet/src/geo/crs/CRS.EPSG4326.js
 //=require ../../../lib/leaflet/src/map/Map.js
+//=require ../../../lib/leaflet/src/layer/Layer.js
 
 // EPSG3395
 //=require ../../../lib/leaflet/src/geo/projection/Projection.Mercator.js
 //=require ../../../lib/leaflet/src/geo/crs/CRS.EPSG3395.js
+
+// Grid Layer
+//=require ../../../lib/leaflet/src/layer/tile/GridLayer.js
 
 // TileLayer
 //=require ../../../lib/leaflet/src/layer/tile/TileLayer.js
 
 // TileLayerWMS
 //=require ../../../lib/leaflet/src/layer/tile/TileLayer.WMS.js
-
-// TileLayerCanvas
-//=require ../../../lib/leaflet/src/layer/tile/TileLayer.Canvas.js
 
 // ImageOverlay
 //=require ../../../lib/leaflet/src/layer/ImageOverlay.js
@@ -46,6 +47,7 @@
 
 // Popup
 //=require ../../../lib/leaflet/src/layer/Popup.js
+//=require ../../../lib/leaflet/src/layer/Layer.Popup.js
 //=require ../../../lib/leaflet/src/layer/marker/Marker.Popup.js
 
 // LayerGroup:
@@ -55,15 +57,8 @@
 //=require ../../../lib/leaflet/src/layer/FeatureGroup.js
 
 // Path
+//=require ../../../lib/leaflet/src/layer/vector/Renderer.js
 //=require ../../../lib/leaflet/src/layer/vector/Path.js
-//=require ../../../lib/leaflet/src/layer/vector/Path.SVG.js
-//=require ../../../lib/leaflet/src/layer/vector/Path.Popup.js
-
-// PathVML
-//=require ../../../lib/leaflet/src/layer/vector/Path.VML.js
-
-// PathCanvas
-//=require ../../../lib/leaflet/src/layer/vector/canvas/Path.Canvas.js
 
 // Polyline
 //=require ../../../lib/leaflet/src/geometry/LineUtil.js
@@ -73,23 +68,23 @@
 //=require ../../../lib/leaflet/src/geometry/PolyUtil.js
 //=require ../../../lib/leaflet/src/layer/vector/Polygon.js
   
-// MultiPoly
-//=require ../../../lib/leaflet/src/layer/vector/MultiPoly.js
-
 // Rectangle
 //=require ../../../lib/leaflet/src/layer/vector/Rectangle.js
-
-// Circle
-//=require ../../../lib/leaflet/src/layer/vector/Circle.js
 
 // CircleMarker
 //=require ../../../lib/leaflet/src/layer/vector/CircleMarker.js
 
-// VectorsCanvas
-//=require ../../../lib/leaflet/src/layer/vector/canvas/Polyline.Canvas.js
-//=require ../../../lib/leaflet/src/layer/vector/canvas/Polygon.Canvas.js
-//=require ../../../lib/leaflet/src/layer/vector/canvas/Circle.Canvas.js
-//=require ../../../lib/leaflet/src/layer/vector/canvas/CircleMarker.Canvas.js
+// Circle
+//=require ../../../lib/leaflet/src/layer/vector/Circle.js
+
+// SVG
+//=require ../../../lib/leaflet/src/layer/vector/SVG.js
+
+// VML
+//=require ../../../lib/leaflet/src/layer/vector/SVG.VML.js
+
+// Canvas
+//=require ../../../lib/leaflet/src/layer/vector/Canvas.js
 
 // GeoJSON
 //=require ../../../lib/leaflet/src/layer/GeoJSON.js
@@ -101,12 +96,16 @@
 //=require ../../../lib/leaflet/src/map/handler/Map.Drag.js
 
 // MouseZoom
+//=require ../../../lib/leaflet/src/dom/DomEvent.js
+//=require ../../../lib/leaflet/src/core/Handler.js
 //=require ../../../lib/leaflet/src/map/handler/Map.DoubleClickZoom.js
 //=require ../../../lib/leaflet/src/map/handler/Map.ScrollWheelZoom.js
 
 // TouchZoom
+//=require ../../../lib/leaflet/src/dom/DomEvent.js
 //=require ../../../lib/leaflet/src/dom/DomEvent.DoubleTap.js
 //=require ../../../lib/leaflet/src/dom/DomEvent.Pointer.js
+//=require ../../../lib/leaflet/src/core/Handler.js
 //=require ../../../lib/leaflet/src/map/handler/Map.TouchZoom.js
 //=require ../../../lib/leaflet/src/map/handler/Map.Tap.js
 
@@ -124,15 +123,19 @@
 //=require ../../../lib/leaflet/src/control/Control.Zoom.js
 
 // ControlAttrib
+//=require ../../../lib/leaflet/src/control/Control.js
 //=require ../../../lib/leaflet/src/control/Control.Attribution.js
 
 // ControlScale
+//=require ../../../lib/leaflet/src/control/Control.js
 //=require ../../../lib/leaflet/src/control/Control.Scale.js
 
 // ControlLayers
+//=require ../../../lib/leaflet/src/control/Control.js
 //=require ../../../lib/leaflet/src/control/Control.Layers.js
 
 // AnimationPan
+//=require ../../../lib/leaflet/src/dom/DomEvent.js
 //=require ../../../lib/leaflet/src/dom/PosAnimation.js
 //=require ../../../lib/leaflet/src/map/anim/Map.PanAnimation.js
 
@@ -141,7 +144,6 @@
 
 // AnimationZoom
 //=require ../../../lib/leaflet/src/map/anim/Map.ZoomAnimation.js
-//=require ../../../lib/leaflet/src/layer/tile/TileLayer.Anim.js
 
 // Geolocation
 //=require ../../../lib/leaflet/src/map/ext/Map.Geolocation.js
