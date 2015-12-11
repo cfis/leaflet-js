c38701f24c2599604f437bc4bf4f94e1f01e61c1
Charlie Savage <cfis@zerista.com>
2015-12-10 20:19:44 -0700

    Fix case in includes.

--- vendor/assets/leaflet.js
+++ vendor/assets/leaflet.js
@@ -1,155 +1,155 @@
 // The core of the library, including OOP, events, DOM facilities, basic units,
 // projections (EPSG:3857 and EPSG:4326) and the base Map class.
-//=require leaflet/src/Leaflet.js
-//=require leaflet/src/core/Util.js
-//=require leaflet/src/core/Class.js
-//=require leaflet/src/core/Events.js
-//=require leaflet/src/core/Browser.js
-//=require leaflet/src/geometry/Point.js
-//=require leaflet/src/geometry/Bounds.js
-//=require leaflet/src/geometry/Transformation.js
-//=require leaflet/src/dom/DomUtil.js
-//=require leaflet/src/geo/LatLng.js
-//=require leaflet/src/geo/LatLngBounds.js
-//=require leaflet/src/geo/projection/Projection.js
-//=require leaflet/src/geo/projection/Projection.SphericalMercator.js
-//=require leaflet/src/geo/projection/Projection.LonLat.js
-//=require leaflet/src/geo/crs/CRS.js
-//=require leaflet/src/geo/crs/CRS.Simple.js
-//=require leaflet/src/geo/crs/CRS.EPSG3857.js
-//=require leaflet/src/geo/crs/CRS.EPSG4326.js
-//=require leaflet/src/map/Map.js
+//=require Leaflet/src/Leaflet.js
+//=require Leaflet/src/core/Util.js
+//=require Leaflet/src/core/Class.js
+//=require Leaflet/src/core/Events.js
+//=require Leaflet/src/core/Browser.js
+//=require Leaflet/src/geometry/Point.js
+//=require Leaflet/src/geometry/Bounds.js
+//=require Leaflet/src/geometry/Transformation.js
+//=require Leaflet/src/dom/DomUtil.js
+//=require Leaflet/src/geo/LatLng.js
+//=require Leaflet/src/geo/LatLngBounds.js
+//=require Leaflet/src/geo/projection/Projection.js
+//=require Leaflet/src/geo/projection/Projection.SphericalMercator.js
+//=require Leaflet/src/geo/projection/Projection.LonLat.js
+//=require Leaflet/src/geo/crs/CRS.js
+//=require Leaflet/src/geo/crs/CRS.Simple.js
+//=require Leaflet/src/geo/crs/CRS.EPSG3857.js
+//=require Leaflet/src/geo/crs/CRS.EPSG4326.js
+//=require Leaflet/src/map/Map.js
 
 // EPSG3395
-//=require leaflet/src/geo/projection/Projection.Mercator.js
-//=require leaflet/src/geo/crs/CRS.EPSG3395.js
+//=require Leaflet/src/geo/projection/Projection.Mercator.js
+//=require Leaflet/src/geo/crs/CRS.EPSG3395.js
 
 // TileLayer
-//=require leaflet/src/layer/tile/TileLayer.js
+//=require Leaflet/src/layer/tile/TileLayer.js
 
 // TileLayerWMS
-//=require leaflet/src/layer/tile/TileLayer.WMS.js
+//=require Leaflet/src/layer/tile/TileLayer.WMS.js
 
 // TileLayerCanvas
-//=require leaflet/src/layer/tile/TileLayer.Canvas.js
+//=require Leaflet/src/layer/tile/TileLayer.Canvas.js
 
 // ImageOverlay
-//=require leaflet/src/layer/ImageOverlay.js
+//=require Leaflet/src/layer/ImageOverlay.js
 
 // Marker
-//=require leaflet/src/layer/marker/Icon.js
-//=require leaflet/src/layer/marker/Icon.Default.js
-//=require leaflet/src/layer/marker/Marker.js
+//=require Leaflet/src/layer/marker/Icon.js
+//=require Leaflet/src/layer/marker/Icon.Default.js
+//=require Leaflet/src/layer/marker/Marker.js
 
 // DivIcon
-//=require leaflet/src/layer/marker/DivIcon.js
+//=require Leaflet/src/layer/marker/DivIcon.js
 
 // Popup
-//=require leaflet/src/layer/Popup.js
-//=require leaflet/src/layer/marker/Marker.Popup.js
+//=require Leaflet/src/layer/Popup.js
+//=require Leaflet/src/layer/marker/Marker.Popup.js
 
 // LayerGroup:
-//=require leaflet/src/layer/LayerGroup.js
+//=require Leaflet/src/layer/LayerGroup.js
 
 // FeatureGroup
-//=require leaflet/src/layer/FeatureGroup.js
+//=require Leaflet/src/layer/FeatureGroup.js
 
 // Path
-//=require leaflet/src/layer/vector/Path.js
-//=require leaflet/src/layer/vector/Path.SVG.js
-//=require leaflet/src/layer/vector/Path.Popup.js
+//=require Leaflet/src/layer/vector/Path.js
+//=require Leaflet/src/layer/vector/Path.SVG.js
+//=require Leaflet/src/layer/vector/Path.Popup.js
 
 // PathVML
-//=require leaflet/src/layer/vector/Path.VML.js
+//=require Leaflet/src/layer/vector/Path.VML.js
 
 // PathCanvas
-//=require leaflet/src/layer/vector/canvas/Path.Canvas.js
+//=require Leaflet/src/layer/vector/canvas/Path.Canvas.js
 
 // Polyline
-//=require leaflet/src/geometry/LineUtil.js
-//=require leaflet/src/layer/vector/Polyline.js
+//=require Leaflet/src/geometry/LineUtil.js
+//=require Leaflet/src/layer/vector/Polyline.js
 
 // Polygon
-//=require leaflet/src/geometry/PolyUtil.js
-//=require leaflet/src/layer/vector/Polygon.js
+//=require Leaflet/src/geometry/PolyUtil.js
+//=require Leaflet/src/layer/vector/Polygon.js
 
 // MultiPoly
-//=require leaflet/src/layer/vector/MultiPoly.js
+//=require Leaflet/src/layer/vector/MultiPoly.js
 
 // Rectangle
-//=require leaflet/src/layer/vector/Rectangle.js
+//=require Leaflet/src/layer/vector/Rectangle.js
 
 // Circle
-//=require leaflet/src/layer/vector/Circle.js
+//=require Leaflet/src/layer/vector/Circle.js
 
 // CircleMarker
-//=require leaflet/src/layer/vector/CircleMarker.js
+//=require Leaflet/src/layer/vector/CircleMarker.js
 
 // VectorsCanvas
-//=require leaflet/src/layer/vector/canvas/Polyline.Canvas.js
-//=require leaflet/src/layer/vector/canvas/Polygon.Canvas.js
-//=require leaflet/src/layer/vector/canvas/Circle.Canvas.js
-//=require leaflet/src/layer/vector/canvas/CircleMarker.Canvas.js
+//=require Leaflet/src/layer/vector/canvas/Polyline.Canvas.js
+//=require Leaflet/src/layer/vector/canvas/Polygon.Canvas.js
+//=require Leaflet/src/layer/vector/canvas/Circle.Canvas.js
+//=require Leaflet/src/layer/vector/canvas/CircleMarker.Canvas.js
 
 // GeoJSON
-//=require leaflet/src/layer/GeoJSON.js
+//=require Leaflet/src/layer/GeoJSON.js
 
 // MapDrag
-//=require leaflet/src/dom/DomEvent.js
-//=require leaflet/src/dom/Draggable.js
-//=require leaflet/src/core/Handler.js
-//=require leaflet/src/map/handler/Map.Drag.js
+//=require Leaflet/src/dom/DomEvent.js
+//=require Leaflet/src/dom/Draggable.js
+//=require Leaflet/src/core/Handler.js
+//=require Leaflet/src/map/handler/Map.Drag.js
 
 // MouseZoom
-//=require leaflet/src/dom/DomEvent.js
-//=require leaflet/src/core/Handler.js
-//=require leaflet/src/map/handler/Map.DoubleClickZoom.js
-//=require leaflet/src/map/handler/Map.ScrollWheelZoom.js
+//=require Leaflet/src/dom/DomEvent.js
+//=require Leaflet/src/core/Handler.js
+//=require Leaflet/src/map/handler/Map.DoubleClickZoom.js
+//=require Leaflet/src/map/handler/Map.ScrollWheelZoom.js
 
 // TouchZoom
-//=require leaflet/src/dom/DomEvent.js
-//=require leaflet/src/dom/DomEvent.DoubleTap.js
-//=require leaflet/src/dom/DomEvent.Pointer.js
-//=require leaflet/src/core/Handler.js
-//=require leaflet/src/map/handler/Map.TouchZoom.js
-//=require leaflet/src/map/handler/Map.Tap.js
+//=require Leaflet/src/dom/DomEvent.js
+//=require Leaflet/src/dom/DomEvent.DoubleTap.js
+//=require Leaflet/src/dom/DomEvent.Pointer.js
+//=require Leaflet/src/core/Handler.js
+//=require Leaflet/src/map/handler/Map.TouchZoom.js
+//=require Leaflet/src/map/handler/Map.Tap.js
 
 // BoxZoom
-//=require leaflet/src/map/handler/Map.BoxZoom.js
+//=require Leaflet/src/map/handler/Map.BoxZoom.js
 
 // Keyboard
-//=require leaflet/src/map/handler/Map.Keyboard.js
+//=require Leaflet/src/map/handler/Map.Keyboard.js
 
 // MarkerDrag
-//=require leaflet/src/layer/marker/Marker.Drag.js
+//=require Leaflet/src/layer/marker/Marker.Drag.js
 
 // ControlZoom
-//=require leaflet/src/control/Control.js
-//=require leaflet/src/control/Control.Zoom.js
+//=require Leaflet/src/control/Control.js
+//=require Leaflet/src/control/Control.Zoom.js
 
 // ControlAttrib
-//=require leaflet/src/control/Control.js
-//=require leaflet/src/control/Control.Attribution.js
+//=require Leaflet/src/control/Control.js
+//=require Leaflet/src/control/Control.Attribution.js
 
 // ControlScale
-//=require leaflet/src/control/Control.js
-//=require leaflet/src/control/Control.Scale.js
+//=require Leaflet/src/control/Control.js
+//=require Leaflet/src/control/Control.Scale.js
 
 // ControlLayers
-//=require leaflet/src/control/Control.js
-//=require leaflet/src/control/Control.Layers.js
+//=require Leaflet/src/control/Control.js
+//=require Leaflet/src/control/Control.Layers.js
 
 // AnimationPan
-//=require leaflet/src/dom/DomEvent.js
-//=require leaflet/src/dom/PosAnimation.js
-//=require leaflet/src/map/anim/Map.PanAnimation.js
+//=require Leaflet/src/dom/DomEvent.js
+//=require Leaflet/src/dom/PosAnimation.js
+//=require Leaflet/src/map/anim/Map.PanAnimation.js
 
 // AnimationTimer
-//=require leaflet/src/dom/PosAnimation.Timer.js
+//=require Leaflet/src/dom/PosAnimation.Timer.js
 
 // AnimationZoom
-//=require leaflet/src/map/anim/Map.ZoomAnimation.js
-//=require leaflet/src/layer/tile/TileLayer.Anim.js
+//=require Leaflet/src/map/anim/Map.ZoomAnimation.js
+//=require Leaflet/src/layer/tile/TileLayer.Anim.js
 
 // Geolocation
-//=require leaflet/src/map/ext/Map.Geolocation.js
+//=require Leaflet/src/map/ext/Map.Geolocation.js
--- vendor/assets/leaflet.label.js
+++ vendor/assets/leaflet.label.js
@@ -1,11 +1,11 @@
 // Core
-//=require leaflet.label/src/Leaflet.label.js
+//=require Leaflet.label/src/Leaflet.label.js
 
 // Label
-//=require leaflet.label/src/Label.js
-//=require leaflet.label/src/BaseMarkerMethods.js
-//=require leaflet.label/src/Marker.Label.js
-//=require leaflet.label/src/CircleMarker.Label.js
-//=require leaflet.label/src/Path.Label.js
-//=require leaflet.label/src/Map.Label.js
-//=require leaflet.label/src/FeatureGroup.Label.js
+//=require Leaflet.label/src/Label.js
+//=require Leaflet.label/src/BaseMarkerMethods.js
+//=require Leaflet.label/src/Marker.Label.js
+//=require Leaflet.label/src/CircleMarker.Label.js
+//=require Leaflet.label/src/Path.Label.js
+//=require Leaflet.label/src/Map.Label.js
+//=require Leaflet.label/src/FeatureGroup.Label.js
